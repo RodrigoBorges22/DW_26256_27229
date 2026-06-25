51c0a34d2c308c14c36f8e3a7137b1c0484c8d51
Tiago Polidoro <aluno27229@ipt.pt>
2026-06-25 18:04:48 +0100

    CRUD Insciçoes e Updates no layout

---
+++ Pages/Inscricoes/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DW_26256_27229.Data;
+using DW_26256_27229.Models;
+
+namespace DW_26256_27229.Pages_Inscricoes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DW_26256_27229.Data.ApplicationDbContext _context;
+
+        public IndexModel(DW_26256_27229.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Inscricao> Inscricao { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Inscricao = await _context.Inscricoes
+                .Include(i => i.Evento)
+                .Include(i => i.Utilizador).ToListAsync();
+        }
+    }
+}
