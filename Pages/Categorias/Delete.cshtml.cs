48df56db4243dcf88194f41a135ddc2f27718014
Tiago Polidoro <aluno27229@ipt.pt>
2026-06-26 00:39:20 +0100

    Validar as operações de remoção/eliminação de informação

--- Pages/Categorias/Delete.cshtml.cs
+++ Pages/Categorias/Delete.cshtml.cs
@@ -1,53 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using DW_26256_27229.Data;
 using DW_26256_27229.Models;
-
 namespace DW_26256_27229.Pages_Categorias
 {
     public class DeleteModel : PageModel
     {
         private readonly DW_26256_27229.Data.ApplicationDbContext _context;
-
         public DeleteModel(DW_26256_27229.Data.ApplicationDbContext context)
         {
             _context = context;
         }
-
         [BindProperty]
         public Categoria Categoria { get; set; } = default!;
-
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
+            if (id == null) return NotFound();
             var categoria = await _context.Categorias.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (categoria is not null)
-            {
-                Categoria = categoria;
-
-                return Page();
-            }
-
+            if (categoria != null) { Categoria = categoria; return Page(); }
             return NotFound();
         }
-
         public async Task<IActionResult> OnPostAsync(int? id)
         {
-            if (id == null)
+            if (id == null) return NotFound();
+            var temEventos = await _context.Eventos.AnyAsync(e => e.CategoriaId == id);
+            if (temEventos)
             {
-                return NotFound();
+                ModelState.AddModelError(string.Empty, "Não é possível eliminar a categoria porque tem eventos associados.");
+                Categoria = await _context.Categorias.FindAsync(id);
+                return Page();
             }
-
             var categoria = await _context.Categorias.FindAsync(id);
             if (categoria != null)
             {
@@ -55,7 +37,6 @@ namespace DW_26256_27229.Pages_Categorias
                 _context.Categorias.Remove(Categoria);
                 await _context.SaveChangesAsync();
             }
-
             return RedirectToPage("./Index");
         }
     }
