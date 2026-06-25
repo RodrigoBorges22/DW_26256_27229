e2ec81afe3353e3761bf5cd9baadf41af3434586
FaiscaMC <RodrigoCruz.Borges@hotmail.com>
2026-06-25 19:19:02 +0100

    Criar API Controller para Categorias

---
+++ Controllers/CategoriasApiController.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DW_26256_27229.Data;
+using DW_26256_27229.Models;
+
+namespace DW_26256_27229.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CategoriasApi (Ler todas as categorias)
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        // GET: api/CategoriasApi/5 (Ler uma categoria pelo ID)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+        }
+
+        // POST: api/CategoriasApi (Criar nova categoria)
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCategoria), new { id = categoria.Id }, categoria);
+        }
+
+        // PUT: api/CategoriasApi/5 (Editar uma categoria)
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Categorias.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/CategoriasApi/5 (Apagar uma categoria)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
