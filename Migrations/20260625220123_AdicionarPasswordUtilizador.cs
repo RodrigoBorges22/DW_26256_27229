fac38cb4f201bc9422d132a6bbd22dd466d15f8c
FaiscaMC <RodrigoCruz.Borges@hotmail.com>
2026-06-25 23:04:25 +0100

    Adicional coluna de Password na tabela Utilizadores na Base de Dados

---
+++ Migrations/20260625220123_AdicionarPasswordUtilizador.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DW_26256_27229.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarPasswordUtilizador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Utilizadores",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Utilizadores",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "123");
+
+            migrationBuilder.UpdateData(
+                table: "Utilizadores",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "123");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Utilizadores");
+        }
+    }
+}
