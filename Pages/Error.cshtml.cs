cbe53ca94def90d1c19f7eb19c096bb4da1e4d91
Tiago Polidoro <aluno27229@ipt.pt>
2026-06-25 23:26:30 +0100

    Páginas de Erro e update do Sobre

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -1,20 +1,10 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace DW_26256_27229.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
-public class ErrorModel : PageModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-    public void OnGet()
-    {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-    }
+[ResponseCache(Duration=0,Location=ResponseCacheLocation.None,NoStore=true)]
+public class ErrorModel:PageModel{
+public int? StatusCode{get;set;}
+public void OnGet(int? id){
+StatusCode=id;
 }
-
+}
