61b6a159c0ec203d5a264245903bb1d8f99b7524
FaiscaMC <RodrigoCruz.Borges@hotmail.com>
2026-06-25 23:11:52 +0100

    Ligar formulário de Registo à base de dados

--- Pages/Registo.cshtml.cs
+++ Pages/Registo.cshtml.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DW_26256_27229.Data;
+using DW_26256_27229.Models;
 
 namespace DW_26256_27229.Pages
 {
     public class RegistoModel : PageModel
     {
+        // 1. ponte para a base de dados
+        private readonly ApplicationDbContext _context;
+
+        public RegistoModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty(SupportsGet = true)]
         public string Tipo { get; set; }
 
@@ -25,10 +35,33 @@ namespace DW_26256_27229.Pages
             }
         }
 
-        public IActionResult OnPost()
+        // 2. O botão "Criar Conta" aciona este método
+        public async Task<IActionResult> OnPostAsync()
         {
-            // Mais tarde, meter aqui a lógica de ligar à API/Base de Dados para gravar o utilizador
-            return Page();
+            // Verifica se o email já existe na base de dados
+            var emailJaExiste = _context.Utilizadores.Any(u => u.Email == Email);
+            if (emailJaExiste)
+            {
+                // Devolve um erro visual debaixo da caixa do email
+                ModelState.AddModelError("Email", "Este email já está registado na plataforma.");
+                return Page();
+            }
+
+            // Constrói o novo utilizador com os dados do formulário
+            var novoUtilizador = new Utilizador
+            {
+                Nome = Nome,
+                Email = Email,
+                Password = Password, // Num projeto empresarial real, aqui encriptáva-se a password, decidimos não encriptar
+                TipoUtilizador = Tipo
+            };
+
+            // Guarda na base de dados
+            _context.Utilizadores.Add(novoUtilizador);
+            await _context.SaveChangesAsync();
+
+            // Depois de criar conta com sucesso, atira o utilizador para a página de Login
+            return RedirectToPage("/Login", new { tipo = Tipo });
         }
     }
 }
