f12e86cddf410776dea8b1c2799b09e0a18f2c66
FaiscaMC <RodrigoCruz.Borges@hotmail.com>
2026-06-26 17:58:46 +0100

    Concluir a renovação da interface (UI) de toda a área de gestão

---
+++ Migrations/20260626154619_AdicionarUtilizadorId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DW_26256_27229.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarUtilizadorId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
