b928bdd8602781c38fa6402e503db76c1537fa3c
Tiago Polidoro <aluno27229@ipt.pt>
2026-06-27 15:16:43 +0100

    Implementa o controlo de lotação, a badge "Lotado" e integra mensagem de feedback ao tentar inscrever num evento lotado

--- Pages/Eventos/Index.cshtml.cs
+++ Pages/Eventos/Index.cshtml.cs
@@ -14,7 +14,7 @@ namespace DW_26256_27229.Pages_Eventos
         {
             var role = User.FindFirst(ClaimTypes.Role)?.Value;
             var userId = int.Parse(User.FindFirst("UtilizadorId")!.Value);
-            var query = _context.Eventos.Include(e => e.Categoria).AsQueryable();
+            var query = _context.Eventos.Include(e => e.Categoria).Include(e => e.Inscricoes).AsQueryable();
             if (role == "Professor") query = query.Where(e => e.UtilizadorId == userId);
             Evento = await query.ToListAsync();
         }
@@ -22,18 +22,20 @@ namespace DW_26256_27229.Pages_Eventos
         {
             var email = User.FindFirst(ClaimTypes.Email)!.Value;
             var alunoId = _context.Utilizadores.First(u => u.Email == email).Id;
-            var existe = await _context.Inscricoes.AnyAsync(i => i.EventoId == id && i.UtilizadorId == alunoId);
-            if (!existe)
-            {
-                _context.Inscricoes.Add(new Inscricao { EventoId = id, UtilizadorId = alunoId, DataInscricao = DateTime.Now });
-                await _context.SaveChangesAsync();
-
-                TempData["MensagemSucesso"] = "Inscrição realizada com sucesso!";
-            }
-            else
+            var evento = await _context.Eventos.Include(e => e.Inscricoes).FirstAsync(e => e.Id == id);
+            if (evento.Inscricoes.Any(i => i.UtilizadorId == alunoId))
             {
                 TempData["MensagemErro"] = "Não é possível inscrever, já te encontras inscrito neste evento.";
+                return RedirectToPage();
             }
+            if (evento.Inscricoes.Count >= evento.VagasMaximas)
+            {
+                TempData["MensagemErro"] = "Este evento já está lotado!";
+                return RedirectToPage();
+            }
+            _context.Inscricoes.Add(new Inscricao { EventoId = id, UtilizadorId = alunoId, DataInscricao = DateTime.Now });
+            await _context.SaveChangesAsync();
+            TempData["MensagemSucesso"] = "Inscrição realizada com sucesso!";
             return RedirectToPage();
         }
     }
